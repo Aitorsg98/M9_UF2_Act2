ad5f1c5aebf95a069308c195930102412a940c8f
formu <formu@ASUS-LAPTOP>
2019-10-26 14:01:53 +0200

    Es modifica el número de fils (de 2 a 4)

--- src/uf2/activitat2.java
+++ src/uf2/activitat2.java
@@ -16,8 +16,8 @@ public class activitat2 {
     Calendar calendario = new GregorianCalendar();
     System.out.println("Inici: "+ calendario.get(Calendar.HOUR_OF_DAY) + ":" + calendario.get(Calendar.MINUTE) +
         ":" + calendario.get(Calendar.SECOND));
-    // Crea un pool de 2 fils
-    final ScheduledExecutorService schExService = Executors.newScheduledThreadPool(2);
+    // Crea un pool de 4 fils
+    final ScheduledExecutorService schExService = Executors.newScheduledThreadPool(4);
     // Crea objecte Runnable.
     final Runnable ob = new activitat2().new ExecutaFil();
     // Programa Fil, s’inicia als 2 segons i després es va executant cada 3 segons
